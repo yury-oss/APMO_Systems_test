46ace90044838391bcc7c554ad9c4bdc9159bb43
yury-oss <infocod4@yandex.ru>
2020-12-19 18:53:48 +0300

    second commit

---
+++ APMO_test/APMO_test/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace APMO_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(109, 11);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 22);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "поиск ведется в";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "выбор папки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 124);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(357, 197);
+            this.treeView1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "regex";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "*.*";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(375, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Остановить поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(372, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Время начала поиска:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(372, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Время конца поиска:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 68);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(357, 50);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(372, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Файлов найдено:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(372, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Всего файлов:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(375, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Пауза";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(375, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 26);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "выполнил:\r\nСучков Ю.Ю.\r\n";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(372, 264);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 26);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Времени прошло:\r\n\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 331);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ APMO_test/APMO_test/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace APMO_test
+{
+    public partial class Form1 : Form
+    {
+        bool stopSearching,pause = false;
+        int countfiles;
+        DateTime starttime;
+        public Form1()
+        {
+           
+            InitializeComponent();
+            richTextBox1.Text = APMO_test.Properties.Settings.Default.path;
+            textBox1.Text = APMO_test.Properties.Settings.Default.regex;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string subpath = richTextBox1.Text;
+            string CurDir = System.IO.Path.GetFullPath(subpath);
+            string myregex = textBox1.Text;
+            char PathSeparator = '\\';
+            countfiles = 0;
+            label3.Text = "Время начала поиска: \n" + DateTime.Now.ToLongTimeString();
+            treeView1.Nodes.Clear();
+            starttime = DateTime.Now;
+
+              OutputTreeView(CurDir, myregex, PathSeparator);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = folderBrowserDialog1.SelectedPath;
+                richTextBox1.Text = filename;
+            }
+        }
+
+       
+
+         async   void OutputTreeView(string CurDir, string myregex, char PathSeparator)
+        {
+            DirectoryInfo DirectoryCurrent = new DirectoryInfo(CurDir);
+            IEnumerable<string> allfiles = DirectoryCurrent.GetFiles(myregex, SearchOption.AllDirectories).Select(s => s.FullName.Substring(s.FullName.LastIndexOf(CurDir))).ToList();
+
+            label6.Text = "Всего файлов: \n" +  allfiles.Count<string>().ToString();
+
+            TreeNode LastNode = null;
+
+            foreach (string PathToFile in allfiles)
+            {
+                if (stopSearching) break;
+
+                    
+                  richTextBox2.Text = System.IO.Path.GetDirectoryName(PathToFile);
+                  string SubPathAgg = string.Empty;
+
+                    foreach (string SubPath in PathToFile.Split(PathSeparator))
+                    {
+                    label8.Text ="Времени прошло \n" + (starttime-DateTime.Now).ToString();
+                        await Task.Run(()=> {
+                            while (pause)
+                            {
+
+                            }
+
+                        });
+                        countfiles++;
+                        label5.Text = "Файлов найдено: \n" + countfiles.ToString();
+                        await Task.Delay(1);
+
+                        SubPathAgg += SubPath + PathSeparator;
+
+                        TreeNode[] Nodes = treeView1.Nodes.Find(SubPathAgg, true);
+
+                        if (Nodes.Length == 0)
+                        {
+
+                            if (LastNode == null)
+                            {
+
+                                LastNode = treeView1.Nodes.Add(SubPathAgg, SubPath);
+                            }
+                            else
+                            {
+                                LastNode = LastNode.Nodes.Add(SubPathAgg, SubPath);
+                            }
+
+                        }
+                        else
+                        {
+                            LastNode = Nodes[0];
+                        }
+
+                    }
+                
+            }
+                label4.Text = "Время конца поиска: \n" + DateTime.Now.ToLongTimeString();
+                stopSearching = false;
+                pause = false;
+                button4.Text = "Пауза";
+
+        }
+
+       
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            stopSearching = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+        private void Form1_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+            
+            APMO_test.Properties.Settings.Default.path = richTextBox1.Text;
+            APMO_test.Properties.Settings.Default.regex = textBox1.Text;
+            APMO_test.Properties.Settings.Default.Save();
+            
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            pause = !pause;
+            if (pause)
+            {
+            button4.Text = "Продолжить";
+
+            }else
+            {
+                button4.Text = "Пауза";
+            }
+        }
+    }
+}
